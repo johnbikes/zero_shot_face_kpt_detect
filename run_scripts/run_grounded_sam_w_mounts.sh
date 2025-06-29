894b3b5bd91e37ecd6fb2bd45ce9498d4fa42d48
johnbikes <474683+johnbikes@users.noreply.github.com>
2025-06-29 15:19:07 -0400

    forgot rm commit

--- run_scripts/run_grounded_sam_w_mounts.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run --gpus all -it --rm --net=host --privileged \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-	-v "${PWD}/Grounded-SAM-2":/home/appuser/Grounded-SAM-2 \
-    -v "${PWD}/scripts":/home/appuser/Grounded-SAM-2/scripts \
-	-e DISPLAY=$DISPLAY \
-	--name=gsa \
-	--ipc=host -it grounded_sam2:1.0
